ec1e983106d9c32778883042b35448f8c39bc3a6
Ayaan Khan <ayankhan161202@gmail.com>
2025-07-29 10:10:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import './components/Navigation'
+
+function App() {
+  return (
+    <>
+      <div> 
+
+        <div className='topbar'>
+           <p>FAST, FREE DELIVERY WITH PRIME </p>
+        </div>
+
+
+        <div className='body'>
+
+          <img src="mainpagepic.JPG" alt="Adidas company logo" className="main-logo" />
+
+       </div>
+
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+const Body = () =>{
+
+return(
+
+       <div className='body'>
+
+          <img src="mainpagepic.JPG" alt="Adidas company logo" className="main-logo" />
+
+       </div>
+);
+
+}
+
+export default Body;
