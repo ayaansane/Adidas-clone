131adf94cd25557152c44fe3104d72462727dc5f
Ayaan Khan <ayankhan161202@gmail.com>
2025-07-30 23:10:58 +0530

    updated footer structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Navigation from './components/Navigation';
 import Body from './components/Body';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navigation />
       <Body />
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,142 @@
+const Footer = () =>{
+
+
+    return (
+<>
+
+
+{/* bluebox*/}
+
+<div className="bluebox">
+<h2>JOIN OUR ADICLUB & GET 15% OFF <button> SIGN UP FOR FREE → </button> </h2> 
+</div>
+
+
+{/* all lists*/}
+<div className="listbox"> 
+
+<div>
+<h4> PRODUCTS </h4>
+<ul>
+    
+    <li> Shoes</li>
+    <li> Clothing</li>
+    <li> Acessories</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+
+
+
+</ul>
+</div>
+
+
+<div>
+<h4> SPORTS </h4>
+<ul>
+    
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+</ul>
+</div>
+
+<div>
+<h4> COLLECTIONS </h4>
+<ul>
+    
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+</ul>
+</div>
+
+<div>
+<h4> SUPPORT </h4>
+<ul>
+    
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+</ul>
+</div>
+
+<div>
+<h4> COMPANY INFO </h4>
+<ul>
+    
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+    <li> Gift Cards</li>
+</ul>
+</div>
+<div>
+<h4> FOLLOW US </h4>
+<ul>
+    
+    <li> <button> Insta </button></li>
+    <li> <button> Facebook </button></li>
+    <li> <button> X </button></li>
+    <li> <button> Pinterest</button></li>
+    <li> <button> Youtube </button></li>
+    
+</ul>
+</div>
+
+</div>
+
+
+{/* last bar*/}
+
+<div className="policybar">
+
+    <div class="left">
+      <span>Your Privacy Choices</span>
+      <a href="#">Privacy Policy</a>
+      <a href="#">Terms and Conditions</a>
+    </div>
+
+    <div class="right">
+      <span>© 2025 adidas America, Inc.</span>
+    </div>
+
+    
+</div>
+
+</>
+
+
+    );
+};
+
+export default Footer;
