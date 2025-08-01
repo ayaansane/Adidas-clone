9688646214707e138e22dfaa680519127a0957c6
Ayaan Khan <ayankhan161202@gmail.com>
2025-08-01 11:04:41 +0530

    Updated CSS and JSX for overlay feature

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,35 +1,34 @@
 const Navigation = () => {
   return (
     <>
-      
       {/* Main Navbar */}
       <div className="navigationbar">
         <nav>
-        <div className="logo">
-          <img src="adidas.png" alt="logo" height="60px" width="80px" />
-        </div>
+          <div className="logoleft">
+            <img src="adidas.png" alt="logo" height="60px" width="80px" />
+          </div>
 
-        <ul>
-          <li><a href="#"><b>Mens</b></a></li>
-          <li><a href="#"><b>Womens</b></a></li>
-          <li><a href="#"><b>Kids</b></a></li>
-          <li><a href="#">Back To School</a></li>
-          <li><a href="#">Sale</a></li>
-          <li><a href="#">New & Trending</a></li>
-        </ul>
-        <div>
+          <div className="navmenu">
+            <ul>
+              <li><a href="#"><b>Mens</b></a></li>
+              <li><a href="#"><b>Womens</b></a></li>
+              <li><a href="#"><b>Kids</b></a></li>
+              <li><a href="#">Back To School</a></li>
+              <li><a href="#">Sale</a></li>
+              <li><a href="#">New & Trending</a></li>
+            </ul>
+          </div>
 
-          <button className="login">Login</button>
-        <button className="login">Signup</button>
-
-        </div>
-        <div>
-
-        </div>
-      </nav>
+          <div className="creds">
+            <button className="login">Login</button>
+            <button className="login">Signup</button>
+          </div>
+        </nav>
       </div>
 
-      <div className="midbar"> <p> Save up to 40% on tons of styles.</p> </div>
+      <div className="midbar">
+        <p>Save up to 40% on tons of styles.</p>
+      </div>
     </>
   );
 };
